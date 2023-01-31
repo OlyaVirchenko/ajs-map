e18bfe239a1f932274adb0734095f1c510f734f0
Olya Virchenko <olya.lyoka02@gmail.com>
2023-01-31 21:21:10 +0300

    Доработка

--- src/js/whatError.js
+++ src/js/whatError.js
@@ -1,6 +1,13 @@
 export default class ErrorRepository {
   constructor() {
-    this.errors = new Map([[1, 'Ошибка 1']]);
+    this.errors = new Map([
+      [1, 'Ошибка 1'],
+      [12, 'Ошибка 12'],
+    ]);
+    this.errors.set(14, 'Ошибка 14');
+    this.errors.set(15, 'Ошибка 15');
+    this.errors.set(25, 'Ошибка 25');
+    this.errors.set(35, 'Ошибка 35');
   }
 
   translate(code) {
